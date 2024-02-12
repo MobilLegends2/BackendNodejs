efa39d3e158f92fdd25da52321495a80e1d17069
ademseddik <adem.seddik@esprit.tn>
2024-02-12 23:14:07 +0100

    adding the forget password with mail

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 // routes/authRoutes.js
 import express from 'express';
-import { register, login,signInUsingToken,unlockSession} from '../controllers/authController.js';
+import { register, login,signInUsingToken,unlockSession, forgotPassword} from '../controllers/authController.js';
 
 const router = express.Router();
-
+router.post('/', forgotPassword);
 router.post('/register', register);
 router.post('/login', login);
 router.post('/sign-in-with-token', signInUsingToken);
