bef5c0efa6fb8c8c2cd7872faa95379ceb8a5e74
aymenzouaoui <aymen.zouaoui@esprit.tn>
2024-02-13 09:57:46 +0100

    mettre ajour le user model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  refreshToken: { type: String }
+  refreshToken: { type: String },
+  avatar: { type: String },
+  status: { type: String },
 });
 
 const User = mongoose.model('User', userSchema);
 
 export default User;
+
