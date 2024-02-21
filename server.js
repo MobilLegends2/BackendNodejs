d33bd32f814e3356a536e53314cb04ab99cc4678
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2024-02-21 13:19:29 +0100

    socket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import cors from "cors";
+import cors from "cors"; // Import the cors middleware
 import { notFoundError, errorHandler } from "./middlewares/error-handler.js";
 import authRoutes from './routes/authRoutes.js';
 import sectionRoutes from './routes/section.js';
@@ -10,24 +10,23 @@ import messageRoutes from './routes/message.js';
 import groupRoutes from './routes/group.js';
 import attachmentRoutes from './routes/attachment.js';
 import categoryRoutes from './routes/category.js';
+import http from 'http';
+import { Server } from 'socket.io';
 
-
-// Creating an express app
 const app = express();
-
-// Setting the port number for the server (default to 9090 if not provided)
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 const PORT = 9090 || process.env.PORT;
-
-// Specifying the MongoDB database name
 const databaseName = 'CrossCHat';
 
-// Enabling debug mode for mongoose
 mongoose.set('debug', true);
-
-// Setting the global Promise library
 mongoose.Promise = global.Promise;
 
-// Connecting to the MongoDB database
 try {
   await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
   console.log(`Connected to ${databaseName}`);
@@ -35,22 +34,13 @@ try {
   console.error(error);
 }
 
-// Enabling Cross-Origin Resource Sharing
-app.use(cors());
+app.use(cors()); // Enable CORS middleware
 
-// Using morgan for logging HTTP requests
 app.use(morgan('dev')); 
-
-// Parsing JSON request bodies
 app.use(express.json());
-
-// Parsing URL-encoded request bodies with extended format
 app.use(express.urlencoded({ extended: true }));
-
-// Serving static files (images) from the 'public/images' directory
 app.use('/img', express.static('public/images'));
 
-// Importing the routes for the 'tests' resource
 app.use('/api/auth', authRoutes);
 app.use('/category', categoryRoutes);
 app.use('/section', sectionRoutes);
@@ -58,13 +48,22 @@ app.use('/', attachmentRoutes);
 app.use('/', groupRoutes);
 app.use('/', conversationRoutes);
 app.use('/', messageRoutes);
-// Using custom middleware for handling 404 errors
 app.use(notFoundError);
-
-// Using custom middleware for handling general errors
 app.use(errorHandler); 
 
-// Starting the server and listening on the specified port
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('new_message', (message) => {
+    console.log('New message:', message);
+    io.emit('new_message', message);
+  });
+});
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
