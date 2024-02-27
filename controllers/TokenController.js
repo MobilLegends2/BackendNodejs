f9842445afc2445aaef0360103d2a0fcfda16d27
aymenzouaoui <aymen.zouaoui@esprit.tn>
2024-02-27 21:19:39 +0100

    Update TokenController.js

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -1,14 +1,31 @@
 import Token from '../models/token.jsoken.js';
 
 export async function createToken(req, res) {
-  try {
-    const { user, application, value, expirationDate } = req.body;
-    const token = await Token.create({ user, application, value, expirationDate });
-    res.status(201).json(token);
-  } catch (error) {
-    res.status(500).json({ error: 'Erreur lors de la création du token' });
+    try {
+      const { user, application, expirationDate, subscriptionType } = req.body;
+      
+      // Génération d'une clé unique pour le token (à utiliser comme secret key pour le JWT)
+      const secretKey = uuidv4();
+      
+      // Création du JWT payload (contenant les informations que vous souhaitez inclure dans le token)
+      const jwtPayload = {
+        userId: user,
+        applicationId: application,
+        subscriptionType,
+        // Autres données si nécessaire
+      };
+  
+      // Génération du JSON Web Token (JWT) à partir du payload et de la clé secrète unique
+      const jwtToken = jwt.sign(jwtPayload, secretKey, { expiresIn: '1h' }); // Exemple : expiration dans 1 heure
+  
+      // Création du token dans la base de données avec la clé secrète unique et le JWT
+      const token = await Token.create({ user, application, secretKey, jwtToken, expirationDate, subscriptionType });
+  
+      res.status(201).json(token);
+    } catch (error) {
+      res.status(500).json({ error: 'Erreur lors de la création du token' });
+    }
   }
-}
 
 export async function getAllTokens(req, res) {
   try {
@@ -57,7 +74,7 @@ export async function deleteToken(req, res) {
 }
 export async function getTokensByUserId(req, res) {
     try {
-      const { userId } = req.params;
+      const { userId } = req.user;
       const tokens = await Token.find({ user: userId });
       res.status(200).json(tokens);
     } catch (error) {
