72371db140bec7f7dd01dfa4243cf64675d5d8ca
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2024-03-11 20:14:28 +0100

    cloudinary

--- models/attachment.js
+++ models/attachment.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 const attachmentSchema = new mongoose.Schema({
-  conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true, index: true },
   url: { type: String, required: true },
   // Other fields you may want to include:
   // type: { type: String, required: true }, // e.g., image, file, etc.
 });
+
 
 const Attachment = mongoose.model('Attachment', attachmentSchema);
 
